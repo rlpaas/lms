0a9484ef90b37de2768c6fc39a8675f32e10efce
Roden L. Paas <rlpaas@mnl.ceu.edu.ph>
2019-11-26 16:05:51 +0800

    add composer adminlte

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ceucc_lms',
+    'dsn' => 'mysql:host=localhost;dbname=cc_lms',
     'username' => 'root',
     'password' => 'PASSWORD',
     'charset' => 'utf8',
